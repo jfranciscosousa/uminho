735337c6e86e861a3c70de862c2f5019616b570d
josebrandao13 <jose.brandao13@hotmail.com>
2016-05-04 12:03:27 +0100

    added trailers and avatars migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430103652) do
+ActiveRecord::Schema.define(version: 20160504105738) do
 
   create_table "albums", force: :cascade do |t|
     t.string "duration"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160430103652) do
     t.string   "actable_type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "trailer"
+    t.string   "avatar"
   end
 
   create_table "reviews", force: :cascade do |t|
