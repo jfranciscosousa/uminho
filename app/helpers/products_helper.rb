0272005280d4ab128bcb4a99f70845867fabe743
José Sousa <josesousa9000@gmail.com>
2016-05-09 13:30:35 +0100

    embelezar um coche

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,12 +1,12 @@
 module ProductsHelper
-  def color_score(score)
-    if score > 75
-      data = "<div class=\"score_green\">#{score.to_i}</div>"
-    elsif score > 50
-      data = "<div class=\"score_yellow\">#{score.to_i}</div>"
-      else data = "<div class=\"score_red\">#{score.to_i}</div>"
+    def color_score(score)
+        data = if score > 75
+                   "<div class=\"Score_green\">#{score.round}</div>"
+               elsif score > 50
+                   "<div class=\"Score_yellow\">#{score.round}</div>"
+               else
+                   "<div class=\"Score_red\">#{score.round}</div>"
+               end
+        data.html_safe
     end
-    data.html_safe
-  end
-
 end
