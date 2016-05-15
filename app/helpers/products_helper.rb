80e065a113f455da2ffd50cfca8436f410bb4130
Cortezz <jospcortez@gmail.com>
2016-05-15 12:29:10 +0100

    Added the rating box into all tables.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,12 +1,2 @@
 module ProductsHelper
-    def color_score(score)
-        data = if score > 75
-                   "<div class=\"Score_green\">#{score.round}</div>"
-               elsif score > 50
-                   "<div class=\"Score_yellow\">#{score.round}</div>"
-               else
-                   "<div class=\"Score_red\">#{score.round}</div>"
-               end
-        data.html_safe
-    end
 end
