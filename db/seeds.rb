4317d44cd3021f7a29fb9bebc4868d191375368f
José Sousa <josesousa9000@gmail.com>
2016-05-30 19:30:45 +0100

    default admin/mod accounts + quickstart db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#default admin
+User.create(email: "admin@critique.com", password:12345678, gender: "Male", role: "admin")
+#default moderator
+User.create(email: "moderator@critique.com", password:12345678, gender: "Male", role: "moderator")
 
 ##### Albums
 
