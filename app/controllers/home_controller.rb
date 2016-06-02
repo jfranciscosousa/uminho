4804f84ed6d7fcf2ef2bf66e9bc426e8a56b49a0
Cortezz <jospcortez@gmail.com>
2016-06-02 11:20:39 +0100

    All content: Top 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,10 @@ class HomeController < ApplicationController
         render 'moderator-home'
       end
     end
-    @games = Product.can_review.games.best.map(&:specific)
-    @movies = Product.can_review.movies.best.map(&:specific)
-    @shows = Product.can_review.shows.best.map(&:specific)
-    @albums = Product.can_review.albums.best.map(&:specific)
+    @games = Product.can_review.games.best.limit(5).map(&:specific)
+    @movies = Product.can_review.movies.best.limit(5).map(&:specific)
+    @shows = Product.can_review.shows.best.limit(5).map(&:specific)
+    @albums = Product.can_review.albums.best.limit(5).map(&:specific)
     @top5 = Product.can_review.hot.limit(5)
     @coming_soon = Product.coming_soon.limit(5).map(&:specific)
   end
